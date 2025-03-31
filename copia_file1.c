1a0e49b024a4fcc5251aad1db699451ece373f1f
antogit-sys <antonioperrgiorgi@gmail.com>
2025-03-31 09:07:35 +0200

    test1

---
+++ copia_file1.c
@@ -0,0 +1,17 @@
+/* Cap 1 (1.5) pag 12
+ *
+ */
+#include <stdio.h>
+int main(void)
+{
+    int c;
+    c = getchar();
+    
+    while(c != EOF){
+        putchar(c);
+        c = getchar();
+    }
+
+
+return 0;
+}
